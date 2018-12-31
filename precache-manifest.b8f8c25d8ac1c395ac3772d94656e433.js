60d21cc7d85aa2cac71cf77d3efd2fdd23357243
Sonny R. Recio <reciosonny@gmail.com>
2018-12-31 21:24:30 +0800

    Generated build files.

---
+++ precache-manifest.b8f8c25d8ac1c395ac3772d94656e433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42aa23d9b936b8ea757b",
+    "url": "/static/js/main.42aa23d9.chunk.js"
+  },
+  {
+    "revision": "104664f3508f584d918a",
+    "url": "/static/js/1.104664f3.chunk.js"
+  },
+  {
+    "revision": "42aa23d9b936b8ea757b",
+    "url": "/static/css/main.1298cdf1.chunk.css"
+  },
+  {
+    "revision": "104664f3508f584d918a",
+    "url": "/static/css/1.3057fb12.chunk.css"
+  },
+  {
+    "revision": "7fc6182f6f3581fcd0c13bf9c8446a5b",
+    "url": "/index.html"
+  }
+];
